2d0eca21060784ca47ba81d4c456b04b04fc3431
Sooyong Lee <sooyongl09@utexas.edu>
2023-10-14 09:20:35 -0500

    Update data rda files and description

---
+++ R/data_desc.r
@@ -0,0 +1,71 @@
+#' continuous.rda
+#'
+#' A data set containing continuous items information.
+#'
+#' @format A data frame with variables:
+#' \describe{
+#'   \item{schid}{School ID}
+#'   \item{id}{Student ID}
+#'   \item{sex}{0 = boys; 1 = girls}
+#'   \item{race}{0 = White; 1 = Others}
+#'   \item{pretest}{Pre test scores}
+#'   \item{stdscore}{Standardized scores}
+#'   \item{stdscore}{Standardized scores}
+#'   \item{trt}{Treatment assignment; 0 = control, 1 = treatment}
+#'   \item{Y}{Outcome}
+#'   \item{q1-q20}{20 continuous items }
+#' }
+#'
+#' @source
+#' @examples
+#' data(continuous)
+#' summary(continuous)
+"continuous"
+
+#' continuous.rda
+#'
+#' A data set containing binary items information.
+#'
+#' @format A data frame with variables:
+#' \describe{
+#'   \item{schid}{School ID}
+#'   \item{id}{Student ID}
+#'   \item{sex}{0 = boys; 1 = girls}
+#'   \item{race}{0 = White; 1 = Others}
+#'   \item{pretest}{Pre test scores}
+#'   \item{stdscore}{Standardized scores}
+#'   \item{stdscore}{Standardized scores}
+#'   \item{trt}{Treatment assignment; 0 = control, 1 = treatment}
+#'   \item{Y}{Outcome}
+#'   \item{q1-q20}{20 binary items }
+#' }
+#'
+#' @source
+#' @examples
+#' data(binary)
+#' summary(binary)
+"binary"
+
+#' graded.rda
+#'
+#' A data set containing graded response items information.
+#'
+#' @format A data frame with variables:
+#' \describe{
+#'   \item{schid}{School ID}
+#'   \item{id}{Student ID}
+#'   \item{sex}{0 = boys; 1 = girls}
+#'   \item{race}{0 = White; 1 = Others}
+#'   \item{pretest}{Pre test scores}
+#'   \item{stdscore}{Standardized scores}
+#'   \item{stdscore}{Standardized scores}
+#'   \item{trt}{Treatment assignment; 0 = control, 1 = treatment}
+#'   \item{Y}{Outcome}
+#'   \item{q1-q20}{20 graded response items }
+#' }
+#'
+#' @source
+#' @examples
+#' data(graded)
+#' summary(graded)
+"graded"
